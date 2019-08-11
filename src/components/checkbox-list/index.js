ca7096a8e0aed68fcf32ba60b1339f6f3aeac93f
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2019-08-12 01:30:53 +0400

    Add expandable param

--- src/components/checkbox-list/index.js
+++ src/components/checkbox-list/index.js
@@ -1,1 +1,17 @@
 import './checkbox-list.scss';
+
+$('.checkbox-list--expandable').each(function() {
+  const $checkbox = $(this);
+  const $checkboxListContent = $('.checkbox-list__list', $checkbox);
+  const $checkboxListTitle = $('.checkbox-list__title', $checkbox);
+
+  const toggleCheckbox = function() {
+    const isExpanded = $checkbox.attr('aria-expanded') === 'true';
+    console.log(isExpanded, !isExpanded);
+    $checkbox.attr('aria-expanded', !isExpanded);
+    $checkboxListContent.slideToggle();
+  };
+
+  $checkboxListTitle.click(toggleCheckbox);
+  toggleCheckbox();
+});
