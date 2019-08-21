322e69ceb85b54c5d63b5a68e4e528a06dd9bec0
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2019-08-22 00:40:08 +0400

    Move getNoun into utils

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,24 +1,7 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 import './dropdown.scss';
-
-const getNoun = function(num, one, two, five) {
-  let lastDigits = num % 100;
-
-  if (lastDigits > 4 && lastDigits < 21) {
-    return five;
-  }
-
-  lastDigits %= 10;
-
-  if (lastDigits === 1) {
-    return one;
-  } else if (lastDigits > 4 || lastDigits === 0) {
-    return five;
-  } else {
-    return two;
-  }
-};
+import { getNoun } from './../../utils';
 
 const getNounFromAttrs = function(count, $element) {
   const one = $element.attr('data-noun-one');
--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,25 @@ const numberWithSpaces = function(n = 0) {
   return parts.join(".");
 };
 
+const getNoun = function(num, one, two, five) {
+  let lastDigits = num % 100;
+
+  if (lastDigits > 4 && lastDigits < 21) {
+    return five;
+  }
+
+  lastDigits %= 10;
+
+  if (lastDigits === 1) {
+    return one;
+  } else if (lastDigits > 4 || lastDigits === 0) {
+    return five;
+  } else {
+    return two;
+  }
+};
+
 export {
   numberWithSpaces,
+  getNoun,
 };
