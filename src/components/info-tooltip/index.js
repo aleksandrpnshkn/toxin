1552c60f2647391d0a953abac2299c1e13a3f0d9
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2019-08-21 17:37:24 +0400

    Add info tooltip component

---
+++ src/components/info-tooltip/index.js
@@ -0,0 +1,7 @@
+import 'jquery-ui/ui/widgets/tooltip';
+import 'jquery-ui/themes/base/core.css';
+import 'jquery-ui/themes/base/tooltip.css';
+
+import './info-tooltip.scss';
+
+$('.info-tooltip').tooltip();
