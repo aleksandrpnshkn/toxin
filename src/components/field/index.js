9f2d3774234d7e34eabd7e039c338c552ef97ff4
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2019-08-23 22:45:57 +0400

    Add masked field. Add jquery.maskedInput dep

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -1,1 +1,14 @@
+import 'jquery.maskedinput/src/jquery.maskedinput';
 import './field.scss';
+
+$('.field__input[data-masked]').each(function() {
+  const $field = $(this);
+  const maskType = $field.data('masked');
+
+  if (maskType === 'date') {
+    $field.mask('99.99.9999', {
+      autoclear: false,
+      placeholder: 'ДД.ММ.ГГГГ',
+    });
+  }
+});
